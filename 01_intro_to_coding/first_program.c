0e35a8fcd9cb298ea490512f2948a4fd3f0b4d87
xavier-c-poly <xcosme1849@floridapoly.edu>
2025-08-20 14:55:18 +0000

    day 1

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,24 @@
+/*
+
+Multi-line comment.
+08/20/2025
+Xavier Cosme
+The first program in C.
+
+*/
+
+// One line comment
+
+// Preprocessor directive
+#include <stdio.h>  // stdio.h - standard input output (.h = header file)
+
+// Create the main function
+int main(void) {
+    printf("Hello, World!\n");
+    // printf("What is your name?: ");
+    // char name[64];
+    // scanf(&name, "%s");
+    // printf("Your name is %s? Nice\n", name);
+
+    return 0;
+}
