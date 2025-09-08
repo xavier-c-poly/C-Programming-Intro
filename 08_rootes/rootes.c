c990e7cc0689df5d137d2e663d902cf8de136ba3
xavier-c-poly <xcosme1849@floridapoly.edu>
2025-09-08 14:52:12 +0000

    stuff with rootes

---
+++ 08_rootes/rootes.c
@@ -0,0 +1,57 @@
+/*
+    The program calculates the rootes of quadratic equation.
+
+    ax^2 + bx + c = 0
+
+    Steps:
+        1. Print the prompt
+        2. Get coefficient
+        3. Calculate the discriminant
+        4. Print rootes
+*/
+
+#include <stdio.h>
+#include <math.h>  // sqrt - function
+
+// Function prototypes
+void printPrompt(char coeff);
+int getCoefficient(void);
+int calculateDiscriminant(int a, int b, int c);
+void printRootes(int a, int b, int c);
+
+int main(void) {
+    printPrompt('a');
+
+    return 0;
+}
+
+void printPrompt(char coeff) {
+    printf("Enter the coefficient %c: ", coeff);
+}
+
+int getCoefficient(void) {
+    int coeff;
+    scanf("%d", &coeff);
+
+    return coeff;
+}
+
+int calculateDiscriminant(int a, int b, int c) {
+    return (b * b) - (4 * a * c);
+}
+
+void printRootes(int a, int b, int c) {
+    int discrim = calculateDiscriminant(a, b, c);
+    if (discrim < 0) {
+        puts("No real rootes");
+    }
+    else if (discrim == 0) {
+        double x = -b / (2 * a);
+        printf("x = %f\n", x);
+    }
+    else if (discrim > 0) {
+        double x1 = (-b + sqrt(discrim)) / (2 * a);  // sqrt - square root
+        double x2 = (-b - sqrt(discrim)) / (2 * a);
+        printf("x1 = %lf; x2 = %lf\n", x1, x2);
+    }
+}
---
+++ 99_test_for_fun/more_pointer_fun.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int findRelativeNumber(float num) {
+    return (num * num) / (num / 2.0);
+}
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Let's try using pointers with functions and arrays" << endl;
+
+
+
+    return 0;    
+}
