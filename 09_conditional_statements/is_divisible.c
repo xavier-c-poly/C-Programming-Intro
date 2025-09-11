2bac2fe780a674833af21526442d2dda4cf25cd4
xavier-c-poly <xcosme1849@floridapoly.edu>
2025-09-11 19:18:37 +0000

    finished is_divisible.c

--- 09_conditional_statements/is_divisible.c
+++ 09_conditional_statements/is_divisible.c
@@ -22,15 +22,42 @@ int main(void) {
     int y = 0;
     printf("y: ");
     scanf("%d", &y);
+    printf("\n");
 
+    if (!is_correct_input(x, y)) {
+        puts("Incorrect input!\n");
+    }
+
+    int numbers[] = {12, 6, 12, 5, 15, 5, x, y};
+
+    for (int i = 0; i < sizeof(numbers) / sizeof(int); i += 2) {
+        if (is_divisible_both(numbers[i], numbers[i+1])) {
+            printf("is_divisible_both(%d, %d) == true\n", numbers[i], numbers[i+1]);
+        } else {
+            printf("is_divisible_both(%d, %d) == false\n", numbers[i], numbers[i+1]);
+        }
+
+        if (is_divisible_either(numbers[i], numbers[i+1])) {
+            printf("is_divisible_either(%d, %d) == true\n", numbers[i], numbers[i+1]);
+        } else {
+            printf("is_divisible_either(%d, %d) == false\n", numbers[i], numbers[i+1]);
+        }
+
+        if (is_divisible_neither(numbers[i], numbers[i+1])) {
+            printf("is_divisible_neither(%d, %d) == true\n", numbers[i], numbers[i+1]);
+        } else {
+            printf("is_divisible_neither(%d, %d) == false\n", numbers[i], numbers[i+1]);
+        }
+
+        printf("\n");
+    }
+
+    /*
     printf("%d\n", is_divisible_both(12, 6));
     printf("%d\n", is_divisible_both(12, 4));
     printf("%d\n", is_divisible_both(15, 5));
     printf("%d\n", is_divisible_both(x, y));
-
-    if (!is_divisible_both(x, y)) {
-        puts("Incorrect input");
-    }
+    */
 
     return 0;
 }
@@ -39,9 +66,15 @@ bool is_divisible_both(int first, int second) {
     return (is_correct_input(first, second)) && (first % second == 0 && first % (second - 3) == 0);
 }
 
-bool is_divisible_either(int first, int second); // is divisible by second OR second minus three
+// is divisible by second OR second minus three
+bool is_divisible_either(int first, int second) {
+    return (is_correct_input(first, second)) && (first % second == 0 || first % (second - 3) == 0);
+}
 
-bool is_divisible_neither(int first, int second); // is NOT divisible by second and is NOT second minus three
+// is NOT divisible by second and is NOT second minus three
+bool is_divisible_neither(int first, int second) {
+    return (is_correct_input(first, second)) && (first % second != 0 && first % (second - 3) != 0);
+}
 
 bool is_correct_input(int first, int second) {
     return second != 0 && second != 3;
