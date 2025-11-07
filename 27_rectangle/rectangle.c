590fa14a4b3749183cb89974d7f993a8c8b4fbe5
xavier-c-poly <xcosme1849@floridapoly.edu>
2025-11-07 15:17:57 +0000

    finished with cool structs lesson!

--- 27_rectangle/rectangle.c
+++ 27_rectangle/rectangle.c
@@ -17,62 +17,62 @@ typedef struct {
 // ---------------------------------------------------
 
 // --------------- FUNCTION PROTOTYPES ---------------
-Rectangle   createRectangle(double width, double height);
-Rectangle   createRectanglePromopt(void);
-double      rectanglePerimeter(Rectangle* rect);
-double      rectangleArea(Rectangle* rect);
-void        printRectangle(Rectangle* rect);
-Room        createRoom(double width_floor, double height_floor);
-void        roomAddWall(Room* room, double width_wall, double height_wall);
+Rectangle   Rectangle_create(double width, double height);
+Rectangle   Rectangle_createPromopt(void);
+double      Rectangle_perimeter(Rectangle* rect);
+double      Rectangle_area(Rectangle* rect);
+void        Rectangle_print(Rectangle* rect);
+Room        Room_create(double width_floor, double height_floor);
+void        Room_addWall(Room* room, double width_wall, double height_wall);
 // TODO
 double      Room_getFloorPerimeter(Room *room);
 double      Room_getWallsPerimeter(Room *room);  // Total perimeter of all the walls
 double      Room_getFloorArea(Room *room);
 double      Room_getWallsArea(Room *room);
-void        printRoom(Room* room);
+void        Room_print(Room* room);
 // ---------------------------------------------------
 
 int main(void) {
     // Create a rectangle r with 15.4 width and 23.5 height
     Rectangle rect1 = {15.4, 23.5};
-    Rectangle rect2 = createRectangle(12.50, 8.00);
+    Rectangle rect2 = Rectangle_create(12.50, 8.00);
 
-    printRectangle(&rect1);
-    printRectangle(&rect2);
+    Rectangle_print(&rect1);
+    Rectangle_print(&rect2);
 
     puts("");
 
-    printf("Perimeter of rect2:\t%.2lf\n", rectanglePerimeter(&rect2));
-    printf("Area of rect2:\t\t%.2lf\n", rectangleArea(&rect2));
+    printf("Perimeter of rect2:\t%.2lf\n", Rectangle_perimeter(&rect2));
+    printf("Area of rect2:\t\t%.2lf\n", Rectangle_area(&rect2));
 
     puts("");
 
     // Create an array of rectangles
     Rectangle arr[3];
-    arr[0] = createRectangle(14.2, 875.3);
-    arr[1] = createRectangle(44.2, 84.3);
-    arr[2] = createRectangle(447.2, 184.3);
+    arr[0] = Rectangle_create(14.2, 875.3);
+    arr[1] = Rectangle_create(44.2, 84.3);
+    arr[2] = Rectangle_create(447.2, 184.3);
 
     for (int i = 0; i < 3; i++) {
-        printf("Rectangle %d: Perimeter %.2lf, Area %.2lf\n", i + 1, rectanglePerimeter(&arr[i]), rectangleArea(&arr[i]));
+        printf("Rectangle %d: Perimeter %.2lf, Area %.2lf\n", i + 1, Rectangle_perimeter(&arr[i]), Rectangle_area(&arr[i]));
     }
 
 
     // Create a room
-    Room living_room = createRoom(12, 12);
-    roomAddWall(&living_room, 13, 13);
-    roomAddWall(&living_room, 16, 14);
-    roomAddWall(&living_room, 13, 12);
-    roomAddWall(&living_room, 14, 11);
+    Room living_room = Room_create(12, 12);
+    Room_addWall(&living_room, 13, 13);
+    Room_addWall(&living_room, 16, 14);
+    Room_addWall(&living_room, 13, 12);
+    Room_addWall(&living_room, 14, 11);
 
     return 0;
 }
 
-Rectangle createRectangle(double width, double height) {
+Rectangle Rectangle_create(double width, double height) {
     return (Rectangle) {width, height};
 }
 
-Rectangle createRectanglePromopt(void) {
+Rectangle Rectangle_createPromopt(void) {
     Rectangle r;
     double w, h;
 
@@ -86,24 +86,24 @@ Rectangle createRectanglePromopt(void) {
     return r;
 }
 
-double rectanglePerimeter(Rectangle* rect) {
+double Rectangle_perimeter(Rectangle* rect) {
     return (rect->width * 2) + (rect->height * 2);
 }
 
-double rectangleArea(Rectangle* rect) {
+double Rectangle_area(Rectangle* rect) {
     return rect->width * rect->height;
 }
 
-void printRectangle(Rectangle* rect) {
+void Rectangle_print(Rectangle* rect) {
     printf("Rectangle width:\t%.2lf\n", rect->width);
     printf("Rectangle height:\t%.2lf\n", rect->height);
-    printf("Perimeter:\t\t%.2lf\n", rectanglePerimeter(rect));
-    printf("Area:\t\t\t%.2lf\n", rectangleArea(rect));
+    printf("Perimeter:\t\t%.2lf\n", Rectangle_perimeter(rect));
+    printf("Area:\t\t\t%.2lf\n", Rectangle_area(rect));
 }
 
-Room createRoom(double width_floor, double height_floor) {
+Room Room_create(double width_floor, double height_floor) {
     // Room room;
-    // room.floor = createRectangle(width_floor, height_floor);
+    // room.floor = Rectangle_create(width_floor, height_floor);
     // room.count_walls = 0;
 
     // return room;
@@ -111,25 +111,49 @@ Room createRoom(double width_floor, double height_floor) {
     return (Room) {.floor = (Rectangle){width_floor, height_floor}, .count_walls = 0};
 }
 
-void roomAddWall(Room* room, double width_wall, double height_wall) {
+void Room_addWall(Room* room, double width_wall, double height_wall) {
     if (room->count_walls >= MAX_WALLS) {
         printf("Cannot have more than %d walls", MAX_WALLS);
         return;
     }
-    room->walls[room->count_walls] = createRectangle(width_wall, height_wall);
+    room->walls[room->count_walls] = Rectangle_create(width_wall, height_wall);
     room->count_walls++;
 }
 
-double Room_getFloorPerimeter(Room *room);
-double Room_getWallsPerimeter(Room *room);  // Total perimeter of all the walls
-double Room_getFloorArea(Room *room);
-double Room_getWallsArea(Room *room);
+double Room_getFloorPerimeter(Room *room) {
+    return Rectangle_perimeter(&room->floor);
+}
 
-void printRoom(Room* room) {
-    //
+double Room_getWallsPerimeter(Room *room) {
+    double perimeter = 0;
 
+    for (int i = 0; i < room->count_walls; i++) {
+        perimeter += Rectangle_perimeter(&room->walls[i]);
+    }
 
+    return perimeter;
+}
 
+double Room_getFloorArea(Room *room) {
+    return Rectangle_area(&room->floor);
+}
 
+double Room_getWallsArea(Room *room) {
+    double area = 0;
 
+    for (int i = 0; i < room->count_walls; i++) {
+        area += Rectangle_area(&room->walls[i]);
+    }
+
+    return area;
+}
+
+void Room_print(Room* room) {
+    printf("Room with floor size %.2lf width by %.2lf length.\n", room->floor.width, room->floor.height);
+
+    for (int i = 0; i < room->count_walls; i++) {
+        printf("Wall %d: ", i + 1);
+        Rectangle_print(&room->walls[i]);
+        puts("");
+    }
 }
